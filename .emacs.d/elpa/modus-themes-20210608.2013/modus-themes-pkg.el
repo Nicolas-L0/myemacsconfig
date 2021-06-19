6226d78838f4c1cc332f05117acaf9292f195e21
Nicolas-L0 <NicEugeo@outlook.com>
2021-06-19 15:27:01 +0800

    initial version of my emacs config file

---
+++ .emacs.d/elpa/modus-themes-20210608.2013/modus-themes-pkg.el
@@ -0,0 +1,12 @@
+(define-package "modus-themes" "20210608.2013" "Highly accessible themes (WCAG AAA)"
+  '((emacs "26.1"))
+  :commit "78838a88e3e9e7681b0b07598b375a5eb55fa060" :authors
+  '(("Protesilaos Stavrou" . "info@protesilaos.com"))
+  :maintainer
+  '("Protesilaos Stavrou" . "info@protesilaos.com")
+  :keywords
+  '("faces" "theme" "accessibility")
+  :url "https://gitlab.com/protesilaos/modus-themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
